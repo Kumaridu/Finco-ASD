882c6d822001a947cb6771e4fbb5dd5326c4488b
Mungun <liab.lhk@gmail.com>
2023-06-20 10:43:03 -0500

    update

---
+++ src/creditcard/CCAccount.java
@@ -0,0 +1,29 @@
+package creditcard;
+
+import framework.account.Account;
+
+public abstract class CCAccount extends Account {
+    private String expDate;
+    private Double lastMonthBalance=0.0;
+    public CCAccount(Double balance, IParty accountOwner,String expDate, Double lastMonthBalance) {
+        super(balance, accountOwner);
+        this.expDate=expDate;
+        this.lastMonthBalance=lastMonthBalance;
+    }
+    public Double getLastMonthBalance(){
+        return lastMonthBalance;
+    }
+    public Double totalMonthlyCredits(){
+        return 0.0;
+    }
+    public Double totalMonthlyCharges(){
+        return 0.0;
+    }
+
+    public abstract Double getNewMonthlybalance();
+    public abstract Double getMonthlyAmountDue();
+    public abstract void addInterest();
+
+
+
+}
