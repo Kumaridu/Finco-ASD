df8b01bcfd5235c7fc013544e21621814cdd140c
Derrick Korku <derrickkorku1@gmail.com>
2023-06-20 17:16:09 -0500

    Update

---
+++ src/framework/AppWindow.java
@@ -0,0 +1,18 @@
+package framework;
+
+import framework.gui.MainWindow;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppWindow extends MainWindow {
+
+    AppWindow(){
+        super("Framework Account Application");
+    }
+    @Override
+    public void ALAPSub(ActionEvent event) {
+
+    }
+}
--- src/framework/Main.java
+++ src/framework/Main.java
@@ -1,44 +1,26 @@
 package framework;
-
-import framework.account.Account;
-import framework.owner.Owner;
-import framework.party.IOrganization;
-import framework.party.PartyCreator;
+import javax.swing.*;
 
 
 public class Main {
     public static void main(String[] args) {
-        IOrganization org = PartyCreator.createOrganization(
-                "Sindhu",
-                "1000 Nth Strt",
-                "Fairfield",
-                "52278",
-                "sindhu@email.com",
-                123
-        );
-
-        Account acc = new ConcAccount(100.0, org);
-
         try {
-            acc.addTransaction("Walmat expense", -20.0);
-            acc.addTransaction("Walmat expense-xxx", -40.0);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            // Add the following code if you want the Look and Feel
+            // to be set to the Look and Feel of the native system.
+
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            }
+            catch (Exception e) {
+            }
+
+            //Create a new instance of our application's frame, and make it visible.
+            (new AppWindow()).setVisible(true);
         }
-
-
-        Account acc2 = new ConcAccount(10.0, org);
-
-        try {
-            acc2.addTransaction("Walmat expense", -20.0);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        catch (Throwable t) {
+            t.printStackTrace();
+            //Ensure the application exits with an error condition.
+            System.exit(1);
         }
-
-
-        Owner.notifyApplyInterest();
-
-
-        System.out.println(Owner.getAccounts().toString());
     }
 }
